c2cf746fd1cafe9452e3839bf3bd8833a8e3b47f
Alfred <somethingfly@somethingfly.com>
2023-11-26 15:56:23 -0800

    commit tree 8b757f2e4d05565f5c12a0c3925fe8ed46c5d65f

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,49 @@
 def admin_login(username, password)
   # your code here
+  if password == "12345" && username == "admin"
+    "Access granted"
+  elsif password == "12345" && username == "ADMIN"
+    "Access granted"
+  else
+    "Access denied"
+  end
 end
 
 def hows_the_weather(temperature)
   # your code here
+  case 
+  when temperature < 40
+    "It's brisk out there!"
+  when temperature < 65
+    "It's a little chilly out there!"
+  when temperature > 85
+    "It's too dang hot out there!"
+  else
+    "It's perfect out there!"
+  end
 end
 
 def fizzbuzz(num)
   # your code here
+  case
+  when num % 3 == 0 && num % 5 == 0 
+    "FizzBuzz"
+  when num % 3 == 0 
+    "Fizz"
+  when num % 5 == 0
+    "Buzz"
+  else 
+    num
+  end
 end
 
 def calculator(operation, num1, num2)
   # your code here
+  case operation
+  when  "+", "-", "*", "/" 
+    num1.send(operation,num2)
+  else
+     puts "Invalid operation!"
+  end
 end
 
